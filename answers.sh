1e892cb3df94b41ac0cb71095a0c17d0706f4a42
Brian Ross Ramos <brramos@data.cs.purdue.edu>
2021-09-17 15:54:46 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,47 @@
 cd ORIGIN
 
 # Task 1 
-
+mv ~/Homework/MA165/HW2.txt ~/Homework/MA165/HW1
 
 # Task 2
-
+rm -rf Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv ~/Memes/Star Wars/fafsa.jpg ~/Memes/Spongebob
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv ~/Programs/Age.cpp ~/Programs/C++
 
 # Task 7
-
+cp ~/Programs/Java ~/Homework/CS180/Project_1 
 
 # Task 8
-
+mv ~/Programs/Count.c ~/Programs/Goodbye.c ~/Programs/C
 
 # Task 9
-
+mv ~/Programs/Name.py ~/Programs/Space.py ~/Programs/Python 3
 
 # Task 10
-
+touch ~Homework/POL141/Essay.txt
 
 # Task 11
-
+ls -l
 
 # Task 12
 : '
 Type your vim/nano customizations below:
 
+set nobackup noswapfile nowritebackup 
+set undofile undodir=~/.vim/undo undolevels=9999
 
+set autoindent expandtab
+set shiftwidth=2 softtabstop=2 tabstop =2
 
+set hlsearch ignorecase incsearch smartcase
 '
